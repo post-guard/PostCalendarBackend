6226b5fc5328f7bcc3befa965c161cde4d5ed0ad
jackfiled <xcrenchangjun@outlook.com>
2023-03-01 17:06:56 +0800

    Init Springboot Project.

---
+++ src/main/java/top/rrricardo/postcalendarbackend/PostCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package top.rrricardo.postcalendarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCalendarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostCalendarBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/rrricardo/postcalendarbackend/PostCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.rrricardo.postcalendarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCalendarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
