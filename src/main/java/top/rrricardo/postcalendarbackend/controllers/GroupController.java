be47e5f25079f7c97fcdaf444ed86af09c56b6e2
Lan_G <2911328695@qq.com>
2023-03-22 19:38:55 +0800

    完善了组织的权限管理部分

--- src/main/java/top/rrricardo/postcalendarbackend/controllers/GroupController.java
+++ src/main/java/top/rrricardo/postcalendarbackend/controllers/GroupController.java
@@ -2,11 +2,17 @@ package top.rrricardo.postcalendarbackend.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import top.rrricardo.postcalendarbackend.annotations.Authorize;
 import top.rrricardo.postcalendarbackend.dtos.ResponseDTO;
+import top.rrricardo.postcalendarbackend.dtos.UserDTO;
+import top.rrricardo.postcalendarbackend.enums.AuthorizePolicy;
+import top.rrricardo.postcalendarbackend.enums.UserPermission;
+import top.rrricardo.postcalendarbackend.mappers.GroupLinkMapper;
 import top.rrricardo.postcalendarbackend.mappers.GroupMapper;
 import top.rrricardo.postcalendarbackend.models.Group;
+import top.rrricardo.postcalendarbackend.models.GroupLink;
 import top.rrricardo.postcalendarbackend.utils.ControllerBase;
-
+import top.rrricardo.postcalendarbackend.components.AuthorizeInterceptor;
 import java.util.List;
 
 @RestController
@@ -15,13 +21,16 @@ public class GroupController extends ControllerBase {
 
 
     private final GroupMapper groupMapper;
+    private final GroupLinkMapper groupLinkMapper;
 
 
-    public GroupController(GroupMapper groupMapper) {
+    public GroupController(GroupMapper groupMapper, GroupLinkMapper groupLinkMapper) {
         this.groupMapper = groupMapper;
+        this.groupLinkMapper = groupLinkMapper;
     }
 
     @GetMapping("/")
+    @Authorize(policy = AuthorizePolicy.ONLY_LOGIN)
     public ResponseEntity<ResponseDTO<List<Group>>> getGroups(){
         var groups = groupMapper.getGroups();
 
@@ -29,6 +38,7 @@ public class GroupController extends ControllerBase {
     }
 
     @GetMapping("/{id}")
+    @Authorize(policy = AuthorizePolicy.ONLY_LOGIN)
     public ResponseEntity<ResponseDTO<Group>> getGroup(@PathVariable(value = "id") int id) {
         var group = groupMapper.getGroupById(id);
 
@@ -40,15 +50,22 @@ public class GroupController extends ControllerBase {
     }
 
     @PostMapping("/")
+    @Authorize(policy = AuthorizePolicy.ONLY_LOGIN)
     public ResponseEntity<ResponseDTO<Group>> createGroup(@RequestBody Group group){
 
         groupMapper.createGroup(group);
+
+        //创建好组织后，将创建者以管理员身份加入组织
+        UserDTO userDTO = AuthorizeInterceptor.getUserDTO();
+        var link = new GroupLink(userDTO.getId(), group.getId(), UserPermission.ADMIN);
+        groupLinkMapper.createGroupLink(link);
 
         return created();
 
     }
 
     @PutMapping("/{id}")
+    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_ADMINISTRATOR)
     public ResponseEntity<ResponseDTO<Group>> updateGroup
             (@PathVariable (value = "id") int id, @RequestBody Group group) throws NullPointerException{
 
@@ -74,6 +91,7 @@ public class GroupController extends ControllerBase {
     }
 
     @DeleteMapping("/{id}")
+    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_ADMINISTRATOR)
     public ResponseEntity<ResponseDTO<Group>> deleteGroup(@PathVariable(value = "id") int id){
 
         var group = groupMapper.getGroupById(id);
