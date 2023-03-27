5906c96189975d83027815e7be7ffd770a8d9048
Lan_G <2911328695@qq.com>
2023-03-27 22:41:19 +0800

    add: 添加地点数据模型

---
+++ src/main/java/top/rrricardo/postcalendarbackend/enums/PlaceType.java
@@ -0,0 +1,10 @@
+package top.rrricardo.postcalendarbackend.enums;
+
+public enum PlaceType {
+
+    //门
+    DOOR,
+
+    //十字路口
+    CROSS_ROADS;
+}
---
+++ src/main/java/top/rrricardo/postcalendarbackend/models/Place.java
@@ -0,0 +1,38 @@
+package top.rrricardo.postcalendarbackend.models;
+
+import top.rrricardo.postcalendarbackend.enums.PlaceType;
+
+public class Place {
+
+    private int id;
+    private String name;
+
+    private int x; //横坐标
+    private int y; //纵坐标
+    private int placeType;
+
+    public Place(String name, int x, int y, PlaceType placeType){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.placeType = placeType.ordinal();
+    }
+
+    public int getId() { return id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getX() { return x; }
+
+    public void setX(int x) {this.x = x; }
+
+    public int getY() { return y; }
+
+    public void setY(int y) { this.y = y; }
+
+    public int getPlaceType() { return placeType; }
+
+    public void setPlaceType(int placeType) { this.placeType = placeType; }
+}
