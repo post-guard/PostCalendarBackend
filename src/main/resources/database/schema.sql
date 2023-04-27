4a4453c135c5fe58053056aee66e27c8593eed6a
jackfiled <xcrenchangjun@outlook.com>
2023-04-27 19:36:25 +0800

    fix: 取消数据库中对于用户名称和电子邮件地址的不合理限制

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -2,8 +2,8 @@ use post_calendar;
 
 create table if not exists `users` (
     `id` int not null auto_increment,
-    `emailAddress` varchar(20) not null ,
-    `username` varchar(20) not null ,
+    `emailAddress` text not null ,
+    `username` text not null ,
     `password` varchar(64) not null ,
     primary key (`id`)
 );
