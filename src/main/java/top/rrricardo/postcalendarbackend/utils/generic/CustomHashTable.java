06e0e88fa420d1b9cbb256b45cc8f2a9129fcff6
jackfiled <xcrenchangjun@outlook.com>
2023-05-14 15:08:16 +0800

    fix: 哈希表rehash是未完全转移数据 从哈希表中remove时丢弃链表中的所有元素

--- src/main/java/top/rrricardo/postcalendarbackend/utils/generic/CustomHashTable.java
+++ src/main/java/top/rrricardo/postcalendarbackend/utils/generic/CustomHashTable.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 
 /**
  * 自行实现的哈希表
+ *
  * @param <K> 键类型
  * @param <V> 值类型
  */
@@ -39,9 +40,9 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
         }
 
         CustomHashTable.loadFactor = loadFactor;
-        table = new LinkedPair<? ,?>[initialCapacity];
+        table = new LinkedPair<?, ?>[initialCapacity];
 
-        threshold = (int)(initialCapacity * loadFactor);
+        threshold = (int) (initialCapacity * loadFactor);
     }
 
     public CustomHashTable(int initialCapacity) {
@@ -57,6 +58,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 获得存储键值对的数量
+     *
      * @return 存储键值对的数量
      */
     public synchronized int getSize() {
@@ -65,6 +67,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 判断表是否为空
+     *
      * @return 为空为真，反之为假
      */
     public synchronized boolean isEmpty() {
@@ -73,6 +76,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 获得一个键对应的值
+     *
      * @param key 指定的键
      * @return 获得的值，如果没有该键为null
      */
@@ -83,9 +87,9 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
         var index = (hash & Integer.MAX_VALUE) % tab.length;
 
         // 遍历链表
-        for(var node = tab[index]; node != null; node = node.nextPair) {
+        for (var node = tab[index]; node != null; node = node.nextPair) {
             if ((node.hashCode == hash) && node.key.equals(key)) {
-                return (V)node.value;
+                return (V) node.value;
             }
         }
         return null;
@@ -93,7 +97,8 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 添加一个键值对
-     * @param key 需要添加的键
+     *
+     * @param key   需要添加的键
      * @param value 需要添加的值
      * @return 如果添加的key已存在，返回被替换的值，反之返回null
      */
@@ -107,7 +112,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
         var index = (hash & Integer.MAX_VALUE) % t.length;
 
         @SuppressWarnings("unchecked")
-        var pair = (LinkedPair<K ,V>)t[index];
+        var pair = (LinkedPair<K, V>) t[index];
         // 首先遍历链表
         // 如果找到就修改值
         for (; pair != null; pair = pair.nextPair) {
@@ -126,6 +131,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 移除指定的键
+     *
      * @param key 需要移除的键
      * @return 返回被移除的对象，如果不存在则返回null
      */
@@ -135,15 +141,15 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
         var index = (hash & Integer.MAX_VALUE) % t.length;
 
         @SuppressWarnings("unchecked")
-        var pair = (LinkedPair<K ,V>)t[index];
+        var pair = (LinkedPair<K, V>) t[index];
         LinkedPair<K, V> previousPair = null;
 
-        for(; pair != null; previousPair = pair, pair = pair.nextPair) {
+        for (; pair != null; previousPair = pair, pair = pair.nextPair) {
             if ((pair.hashCode == hash) && pair.key.equals(key)) {
                 // 找到节点
                 if (previousPair == null) {
                     // 是链表中的头结点
-                    t[index] = null;
+                    t[index] = pair.nextPair;
                 } else {
                     previousPair.nextPair = pair.nextPair;
                 }
@@ -184,13 +190,13 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
             }
             newCapacity = maxArraySize;
         }
-        threshold = (int)(newCapacity * loadFactor);
+        threshold = (int) (newCapacity * loadFactor);
 
         var newTable = new LinkedPair<?, ?>[newCapacity];
         table = newTable;
 
-        for (var i = oldCapacity - 1; i > 0; i--) {
-            for (var old = (LinkedPair<K, V>)oldTable[i]; old != null;) {
+        for (var i = oldCapacity - 1; i >= 0; i--) {
+            for (var old = (LinkedPair<K, V>) oldTable[i]; old != null; ) {
                 var index = (old.hashCode & Integer.MAX_VALUE) % newCapacity;
 
                 var pair = old;
@@ -204,8 +210,9 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 链表中添加一个节点
-     * @param hash 键哈希值
-     * @param key 键
+     *
+     * @param hash  键哈希值
+     * @param key   键
      * @param value 值
      * @param index 数组的索引
      */
@@ -223,7 +230,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
         // 创建新节点
         @SuppressWarnings("unchecked")
-        var pair = (LinkedPair<K ,V>)t[index];
+        var pair = (LinkedPair<K, V>) t[index];
         // 这里是在链表的头部添加节点
         t[index] = new LinkedPair<>(hash, key, value, pair);
         count++;
@@ -237,6 +244,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
     /**
      * 链表法存储哈希表中的值对象
+     *
      * @param <K> 键类型
      * @param <V> 值类型
      */
@@ -252,9 +260,9 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
         /**
          * 下一个节点
          */
-        LinkedPair<K ,V> nextPair;
+        LinkedPair<K, V> nextPair;
 
-        protected LinkedPair(int hash, K key, V value, LinkedPair<K ,V> next) {
+        protected LinkedPair(int hash, K key, V value, LinkedPair<K, V> next) {
             this.hashCode = hash;
             this.key = key;
             this.value = value;
@@ -276,7 +284,6 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
             value = v;
             return v;
         }
-
 
         @Override
         public boolean equals(Object obj) {
@@ -328,7 +335,7 @@ public class CustomHashTable<K, V> implements Iterable<Map.Entry<K, V>> {
 
             if (p != null) {
                 pair = p.nextPair;
-                return (Map.Entry<K, V>)p;
+                return (Map.Entry<K, V>) p;
             }
             throw new NoSuchElementException();
         }
--- src/test/java/top/rrricardo/postcalendarbackend/utils/CustomHashTableTest.java
+++ src/test/java/top/rrricardo/postcalendarbackend/utils/CustomHashTableTest.java
@@ -12,10 +12,51 @@ public class CustomHashTableTest {
     @Test
     void testHashTablePut1() {
         var table = new CustomHashTable<Integer, Integer>();
+        var expect = new Hashtable<Integer, Integer>();
+        var random = new Random();
+
+        for (var i = 0; i < 1000; i++) {
+            var key = random.nextInt();
+            var value = random.nextInt();
+
+            table.put(key, value);
+            expect.put(key, value);
+        }
+
+        Assertions.assertEquals(expect.size(), table.getSize());
+        for (var key : expect.keySet()) {
+            Assertions.assertEquals(expect.get(key), table.get(key));
+        }
+    }
+
+    @Test
+    void testHashTablePut2() {
+        var table = new CustomHashTable<Integer, Integer>(200);
+
+        table.put(2, 3);
+        table.put(3, 4);
+
+        Assertions.assertEquals(3, table.put(2, 5));
+    }
+
+    @Test
+    void testHashTableIsEmpty() {
+        var table = new CustomHashTable<Integer, Integer>();
+
+        Assertions.assertTrue(table.isEmpty());
+
+        table.put(2, 3);
+
+        Assertions.assertFalse(table.isEmpty());
+    }
+
+    @Test
+    void testHashTableRemove() {
+        var table = new CustomHashTable<Integer, Integer>();
         var except = new Hashtable<Integer, Integer>();
         var random = new Random();
 
-        for (var i = 0; i < 50; i++) {
+        for (var i = 0; i < 1000; i++) {
             var key = random.nextInt();
             var value = random.nextInt();
 
@@ -23,19 +64,68 @@ public class CustomHashTableTest {
             except.put(key, value);
         }
 
-        System.out.println("Except: ");
-        for (var pair : except.entrySet()) {
-            System.out.println(pair.getKey() + "-" + pair.getValue());
-        }
-
-        System.out.println("Actual: ");
-        for (var pair : table) {
-            System.out.println(pair.getKey() + "-" + pair.getValue());
-        }
-
         Assertions.assertEquals(except.size(), table.getSize());
         for (var key : except.keySet()) {
             Assertions.assertEquals(except.get(key), table.get(key));
+            Assertions.assertEquals(except.get(key), table.remove(key));
         }
+
+        Assertions.assertTrue(table.isEmpty());
+    }
+
+    @Test
+    void testHashTableClear() {
+        var table = new CustomHashTable<Integer, Integer>();
+        var random = new Random();
+
+        for (var i = 0; i < 1000; i++) {
+            table.put(random.nextInt(), random.nextInt());
+        }
+
+        Assertions.assertEquals(1000, table.getSize());
+
+        table.clear();
+
+        Assertions.assertEquals(0, table.getSize());
+        Assertions.assertTrue(table.isEmpty());
+
+        var expect = new Hashtable<Integer, Integer>();
+
+        for (var i = 0; i < 1000; i++) {
+            var key = random.nextInt();
+            var value = random.nextInt();
+
+            table.put(key, value);
+            expect.put(key, value);
+        }
+
+        Assertions.assertEquals(expect.size(), table.getSize());
+        for (var key : expect.keySet()) {
+            Assertions.assertEquals(expect.get(key), table.get(key));
+        }
+    }
+
+    @Test
+    void testHashTableIterator() {
+        var table = new CustomHashTable<Integer, Integer>();
+        var expect = new Hashtable<Integer, Integer>();
+        var random = new Random();
+
+        for (var i = 0; i < 1000; i++) {
+            var key = random.nextInt();
+            var value = random.nextInt();
+
+            table.put(key, value);
+            expect.put(key, value);
+        }
+
+        Assertions.assertEquals(expect.size(), table.getSize());
+        var counter = 0;
+        for (var pair : table) {
+            counter++;
+            Assertions.assertEquals(expect.get(pair.getKey()), pair.getValue());
+        }
+
+        Assertions.assertEquals(expect.size(), counter);
     }
 }
