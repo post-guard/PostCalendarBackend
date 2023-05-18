b7542368810a7da2d377ca88cfe18835efcc5588
jackfiled <xcrenchangjun@outlook.com>
2023-05-18 21:25:40 +0800

    add: 添加、删除、修改时间段事件的辅助函数和相关错误

---
+++ src/main/java/top/rrricardo/postcalendarbackend/exceptions/TimeSpanEventException.java
@@ -0,0 +1,14 @@
+package top.rrricardo.postcalendarbackend.exceptions;
+
+/**
+ * 时间段事件相关的异常
+ */
+public class TimeSpanEventException extends Exception {
+    public TimeSpanEventException() {
+        super();
+    }
+
+    public TimeSpanEventException(String message) {
+        super(message);
+    }
+}
