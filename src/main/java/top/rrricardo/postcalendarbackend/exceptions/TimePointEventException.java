edcab80dd2d34e6d37560b3566b0bdd406a6cbb1
jackfiled <xcrenchangjun@outlook.com>
2023-05-21 16:00:07 +0800

    add: 时间点事件查询服务和时间点查询异常

---
+++ src/main/java/top/rrricardo/postcalendarbackend/exceptions/TimePointEventException.java
@@ -0,0 +1,14 @@
+package top.rrricardo.postcalendarbackend.exceptions;
+
+/**
+ * 时间点事件相关的异常
+ */
+public class TimePointEventException extends Exception {
+    public TimePointEventException() {
+        super();
+    }
+
+    public TimePointEventException(String message) {
+        super(message);
+    }
+}
--- src/main/java/top/rrricardo/postcalendarbackend/services/TimePointEventService.java
+++ src/main/java/top/rrricardo/postcalendarbackend/services/TimePointEventService.java
@@ -1,45 +1,26 @@
 package top.rrricardo.postcalendarbackend.services;
 
 import top.rrricardo.postcalendarbackend.exceptions.AvlNodeRepeatException;
+import top.rrricardo.postcalendarbackend.exceptions.TimePointEventException;
 import top.rrricardo.postcalendarbackend.exceptions.TimeSpanEventException;
-import top.rrricardo.postcalendarbackend.models.TimeSpanEvent;
+import top.rrricardo.postcalendarbackend.models.TimePointEvent;
 import top.rrricardo.postcalendarbackend.utils.generic.AvlTree;
 import top.rrricardo.postcalendarbackend.utils.generic.CustomHashTable;
 import top.rrricardo.postcalendarbackend.utils.generic.CustomList;
 
 import java.time.LocalDateTime;
 
-public abstract class TimeSpanEventService {
-    /**
-     * 添加一个时间段
-     *
-     * @param event 需要添加的事件
-     */
-    public abstract void addEvent(TimeSpanEvent event) throws TimeSpanEventException;
+/**
+ * 时间点事件服务
+ */
+public abstract class TimePointEventService {
+    public abstract void addEvent(TimePointEvent event);
 
-    /**
-     * 移除一个事件
-     *
-     * @param event 需要移除的事件
-     */
-    public abstract void removeEvent(TimeSpanEvent event) throws TimeSpanEventException;
+    public abstract void removeEvent(TimePointEvent event);
 
-    /**
-     * 更新事件
-     *
-     * @param event 需要更新的事件
-     */
-    public abstract void updateEvent(TimeSpanEvent event) throws TimeSpanEventException;
+    public  abstract void updateEvent(TimePointEvent event);
 
-    /**
-     * 获得指定用户在指定时间范围的事件
-     *
-     * @param id    ID
-     * @param begin 开始时间
-     * @param end   结束时间
-     * @return 事件列表
-     */
-    public abstract CustomList<TimeSpanEvent> queryEvent(int id, LocalDateTime begin, LocalDateTime end) throws TimeSpanEventException;
+    public abstract CustomList<TimePointEvent> queryEvent(int id, LocalDateTime begin, LocalDateTime end);
 
     /**
      * 添加事件辅助函数
@@ -47,8 +28,8 @@ public abstract class TimeSpanEventService {
      * @param forest 需要添加的森林
      * @param id 欲添加的属主ID
      */
-    protected void addEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest, int id)
-            throws TimeSpanEventException {
+    protected void addEventHelper(TimePointEvent event, CustomHashTable<Integer, AvlTree<TimePointEvent>> forest, int id)
+            throws TimePointEventException {
         try {
             var tree = forest.get(id);
 
@@ -62,7 +43,7 @@ public abstract class TimeSpanEventService {
                 tree.insert(event);
             }
         } catch (AvlNodeRepeatException e) {
-            throw new TimeSpanEventException("发生冲突");
+            throw new TimePointEventException(e.getMessage());
         }
     }
 
@@ -72,12 +53,12 @@ public abstract class TimeSpanEventService {
      * @param forest 需要删除的森林
      * @param id 属主ID
      */
-    protected void removeEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
-                                     int id) throws TimeSpanEventException {
+    protected void removeEventHelper(TimePointEvent event, CustomHashTable<Integer, AvlTree<TimePointEvent>> forest,
+                                     int id) throws TimePointEventException {
         var tree = forest.get(id);
 
         if (tree == null) {
-            throw new TimeSpanEventException("欲删除的事件非法：ID：" + id + "不存在");
+            throw new TimePointEventException("欲删除的事件非法：ID：" + id + "不存在");
         }
 
         tree.remove(event);
@@ -89,15 +70,15 @@ public abstract class TimeSpanEventService {
      * @param forest 需要修改的森林
      * @param id 属主ID
      */
-    protected void updateEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
-                                     int id) throws TimeSpanEventException {
+    protected void updateEventHelper(TimePointEvent event, CustomHashTable<Integer, AvlTree<TimePointEvent>> forest,
+                                     int id) throws TimePointEventException {
         var tree = forest.get(id);
 
         if (tree == null) {
-            throw new TimeSpanEventException("欲删除的事件非法：ID：" + id + "不存在");
+            throw new TimePointEventException("欲删除的事件非法：ID：" + id + "不存在");
         }
 
-        TimeSpanEvent oldEvent = null;
+        TimePointEvent oldEvent = null;
 
         for (var item : tree) {
             if (item.getId() == event.getId()) {
@@ -107,16 +88,16 @@ public abstract class TimeSpanEventService {
         }
 
         if (oldEvent == null) {
-            throw new TimeSpanEventException("欲修改的事件不存在");
+            throw new TimePointEventException("欲修改的事件不存在");
         }
 
-        if (oldEvent.getBeginDateTime().equals(event.getBeginDateTime())
-        && oldEvent.getEndDateTime().equals(event.getEndDateTime())) {
+        if (oldEvent.getEndDateTime().equals(event.getEndDateTime())) {
             // 不修改事件的开始和结束时间
             // 直接修改原来的二叉树
             oldEvent.setName(event.getName());
             oldEvent.setDetails(event.getDetails());
             oldEvent.setPlaceId(event.getPlaceId());
+            oldEvent.setType(event.getType());
         } else {
             // 修改事件的开始和结束时间
             // 那就删除事件重新添加
@@ -124,7 +105,7 @@ public abstract class TimeSpanEventService {
             try {
                 tree.insert(event);
             } catch (AvlNodeRepeatException exception) {
-                throw new TimeSpanEventException("发生冲突");
+                throw new TimePointEventException("发生冲突");
             }
         }
     }
@@ -136,27 +117,23 @@ public abstract class TimeSpanEventService {
      * @param begin 开始时间
      * @param end 结束时间
      * @return 在时间段内的事件列表
-     * @throws TimeSpanEventException 查询不存在的用户/组织引发的错误
+     * @throws TimePointEventException 查询不存在的用户/组织引发的错误
      */
-    protected CustomList<TimeSpanEvent> queryEventHelper(CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
+    protected CustomList<TimePointEvent> queryEventHelper(CustomHashTable<Integer, AvlTree<TimePointEvent>> forest,
                                                          int id, LocalDateTime begin, LocalDateTime end)
-            throws TimeSpanEventException {
+            throws TimePointEventException {
         var tree = forest.get(id);
 
         if (tree == null) {
-            throw new TimeSpanEventException("属主：" + id + "不存在");
+            throw new TimePointEventException("属主：" + id + "不存在");
         }
 
-        var beginTimeObj = new TimeSpanEvent();
-        beginTimeObj.setBeginDateTime(begin);
+        var beginTimeObj = new TimePointEvent();
         beginTimeObj.setEndDateTime(begin);
 
-        var endTimeObj = new TimeSpanEvent();
-        endTimeObj.setBeginDateTime(end);
+        var endTimeObj = new TimePointEvent();
         endTimeObj.setEndDateTime(end);
 
         return tree.selectRange(beginTimeObj, endTimeObj);
     }
-
-
 }
