31132fc91d63584079154bde1e2ffeb47b0de982
jackfiled <xcrenchangjun@outlook.com>
2023-05-23 14:13:24 +0800

    refactor: 重构了时间段事件服务

--- src/main/java/top/rrricardo/postcalendarbackend/controllers/TimeSpanEventController.java
+++ src/main/java/top/rrricardo/postcalendarbackend/controllers/TimeSpanEventController.java
@@ -6,10 +6,6 @@ import top.rrricardo.postcalendarbackend.annotations.Authorize;
 import top.rrricardo.postcalendarbackend.dtos.ResponseDTO;
 import top.rrricardo.postcalendarbackend.enums.AuthorizePolicy;
 import top.rrricardo.postcalendarbackend.exceptions.TimeSpanEventException;
-import top.rrricardo.postcalendarbackend.mappers.GroupLinkMapper;
-import top.rrricardo.postcalendarbackend.mappers.GroupMapper;
-import top.rrricardo.postcalendarbackend.mappers.TimeSpanEventMapper;
-import top.rrricardo.postcalendarbackend.mappers.UserMapper;
 import top.rrricardo.postcalendarbackend.models.TimeSpanEvent;
 import top.rrricardo.postcalendarbackend.services.TimeSpanEventService;
 import top.rrricardo.postcalendarbackend.utils.ControllerBase;
@@ -18,29 +14,14 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/timeSpanEvent")
 public class TimeSpanEventController extends ControllerBase {
-    private final UserMapper userMapper;
-    private final GroupMapper groupMapper;
-    private final GroupLinkMapper groupLinkMapper;
-    private final TimeSpanEventMapper eventMapper;
-    private final TimeSpanEventService userTimeSpanEventService;
-    private final TimeSpanEventService groupTimeSpanEventService;
+    private final TimeSpanEventService timeSpanEventService;
 
-    public TimeSpanEventController(TimeSpanEventMapper eventMapper,
-                                   UserMapper userMapper,
-                                   GroupMapper groupMapper,
-                                   GroupLinkMapper groupLinkMapper,
-                                   Map<String, TimeSpanEventService> serviceMap) {
-        this.userMapper = userMapper;
-        this.groupMapper = groupMapper;
-        this.groupLinkMapper = groupLinkMapper;
-        this.eventMapper = eventMapper;
-        this.userTimeSpanEventService = serviceMap.get("userTimeSpanEvent");
-        this.groupTimeSpanEventService = serviceMap.get("groupTimeSpanEvent");
+    public TimeSpanEventController(TimeSpanEventService timeSpanEventService) {
+        this.timeSpanEventService = timeSpanEventService;
     }
 
     @GetMapping("/user/{id}")
@@ -54,11 +35,6 @@ public class TimeSpanEventController extends ControllerBase {
             return badRequest("开始时间晚于结束时间");
         }
 
-        var user = userMapper.getUserById(id);
-        if (user == null) {
-            return notFound("用户不存在");
-        }
-
         var instant = Instant.ofEpochSecond(begin);
         var beginTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 
@@ -66,17 +42,7 @@ public class TimeSpanEventController extends ControllerBase {
         var endTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 
         try {
-            var result = userTimeSpanEventService.queryEvent(id, beginTime, endTime);
-            // 查询当前用户所属组织中的事件
-            var groupLinks = groupLinkMapper.getGroupLinksByUserId(id);
-
-            for (var groupLink : groupLinks) {
-                var eventsInGroup = groupTimeSpanEventService.queryEvent(groupLink.getGroupId(), beginTime, endTime);
-
-                for (var item : eventsInGroup) {
-                    result.add(item);
-                }
-            }
+            var result = timeSpanEventService.queryUserEvent(id, beginTime, endTime);
 
             return ok(result.toList());
         } catch (TimeSpanEventException e) {
@@ -94,27 +60,8 @@ public class TimeSpanEventController extends ControllerBase {
             return badRequest("请求的用户ID和事件用户ID不一致");
         }
 
-        var user = userMapper.getUserById(id);
-        if (user == null) {
-            return notFound("用户不存在");
-        }
-
-        // 判断事件是否冲突
-        if (!userTimeSpanEventService.judgeConflict(id, event)) {
-            return badRequest("同用户中的事件发生冲突");
-        }
-
-        var groupLinks = groupLinkMapper.getGroupLinksByUserId(id);
-
-        for (var groupLink : groupLinks) {
-            // 检查是否同用户所在组织的事件冲突
-            if (!groupTimeSpanEventService.judgeConflict(groupLink.getGroupId(), event)) {
-                return badRequest("同所在组织：" + groupLink.getGroupId() + "的事件冲突");
-            }
-        }
-
         try {
-            userTimeSpanEventService.addEvent(event);
+            timeSpanEventService.addUserEvent(event);
 
             return created(event);
         } catch (TimeSpanEventException e) {
@@ -128,19 +75,12 @@ public class TimeSpanEventController extends ControllerBase {
             @PathVariable int id,
             @RequestBody TimeSpanEvent event
     ) {
-        var user = userMapper.getUserById(id);
-
-        if (user == null) {
-            return notFound("用户不存在");
-        }
-
-        var oldEvent = eventMapper.getEventById(event.getId());
-        if (oldEvent == null) {
-            return notFound("事件不存在");
+        if (id != event.getUserId()) {
+            return badRequest("请求的用户ID和事件用户ID不一致");
         }
 
         try {
-            userTimeSpanEventService.updateEvent(event);
+            timeSpanEventService.updateUserEvent(event);
 
             return ok(event);
         } catch (TimeSpanEventException e) {
@@ -154,24 +94,12 @@ public class TimeSpanEventController extends ControllerBase {
             @PathVariable int id,
             @RequestBody TimeSpanEvent event
     ) {
-        var user = userMapper.getUserById(id);
-
-        if (user == null) {
-            return notFound("用户不存在");
-        }
-
-        var oldEvent = eventMapper.getEventById(event.getId());
-
-        if (oldEvent == null) {
-            return notFound("要删除的事件不存在");
-        }
-
-        if (!event.equals(oldEvent)) {
-            return badRequest("要删除的事件信息同数据库不符");
+        if (id != event.getUserId()) {
+            return badRequest("请求的用户ID和事件用户ID不一致");
         }
 
         try {
-            userTimeSpanEventService.removeEvent(event);
+            timeSpanEventService.removeUserEvent(event);
             return noContent();
         } catch (TimeSpanEventException e) {
             return badRequest(e.getMessage());
@@ -180,7 +108,7 @@ public class TimeSpanEventController extends ControllerBase {
 
     @GetMapping("/group/{id}")
     @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_USER)
-    public ResponseEntity<ResponseDTO<List<TimeSpanEvent>>> getGroupEvent(
+    public ResponseEntity<ResponseDTO<List<TimeSpanEvent>>> getGroupEvents(
             @PathVariable int id,
             @RequestParam long begin,
             @RequestParam long end
@@ -189,18 +117,14 @@ public class TimeSpanEventController extends ControllerBase {
             return badRequest("开始时间晚于结束时间");
         }
 
-        var group = groupMapper.getGroupById(id);
-        if (group == null) {
-            return notFound("组织不存在");
-        }
-
         var instant = Instant.ofEpochSecond(begin);
         var beginTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+
         instant = Instant.ofEpochSecond(end);
         var endTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 
         try {
-            var result = groupTimeSpanEventService.queryEvent(id, beginTime, endTime);
+            var result = timeSpanEventService.queryGroupEvent(id, beginTime, endTime);
 
             return ok(result.toList());
         } catch (TimeSpanEventException e) {
@@ -209,7 +133,7 @@ public class TimeSpanEventController extends ControllerBase {
     }
 
     @PostMapping("/group/{id}")
-    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_USER)
+    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_ADMINISTRATOR)
     public ResponseEntity<ResponseDTO<TimeSpanEvent>> addGroupEvent(
             @PathVariable int id,
             @RequestBody TimeSpanEvent event
@@ -218,26 +142,8 @@ public class TimeSpanEventController extends ControllerBase {
             return badRequest("请求的组织ID和事件组织ID不一致");
         }
 
-        var group = groupMapper.getGroupById(id);
-        if (group == null) {
-            return notFound("请求的组织不存在");
-        }
-
-        // 判断冲突
-        if (!groupTimeSpanEventService.judgeConflict(id, event)) {
-            return badRequest("同组织中的事件发生冲突");
-        }
-
-        var groupLinks = groupLinkMapper.getGroupLinksByGroupId(id);
-
-        for (var groupLink : groupLinks) {
-            if (!userTimeSpanEventService.judgeConflict(groupLink.getUserId(), event)) {
-                return badRequest("同组织中用户：" + groupLink.getUserId() + "的事件发生冲突");
-            }
-        }
-
         try {
-            groupTimeSpanEventService.addEvent(event);
+            timeSpanEventService.addGroupEvent(event);
 
             return created(event);
         } catch (TimeSpanEventException e) {
@@ -246,23 +152,17 @@ public class TimeSpanEventController extends ControllerBase {
     }
 
     @PutMapping("/group/{id}")
-    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_USER)
+    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_ADMINISTRATOR)
     public ResponseEntity<ResponseDTO<TimeSpanEvent>> updateGroupEvent(
             @PathVariable int id,
             @RequestBody TimeSpanEvent event
     ) {
-        var group = groupMapper.getGroupById(id);
-        if (group == null) {
-            return notFound("请求的组织不存在");
-        }
-
-        var oldEvent = eventMapper.getEventById(event.getId());
-        if (oldEvent == null) {
-            return notFound("事件不存在");
+        if (id != event.getGroupId()) {
+            return badRequest("请求的组织ID和事件组织ID不一致");
         }
 
         try {
-            groupTimeSpanEventService.updateEvent(event);
+            timeSpanEventService.updateGroupEvent(event);
 
             return ok(event);
         } catch (TimeSpanEventException e) {
@@ -271,28 +171,17 @@ public class TimeSpanEventController extends ControllerBase {
     }
 
     @DeleteMapping("/group/{id}")
-    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_USER)
+    @Authorize(policy = AuthorizePolicy.CURRENT_GROUP_ADMINISTRATOR)
     public ResponseEntity<ResponseDTO<TimeSpanEvent>> removeGroupEvent(
             @PathVariable int id,
             @RequestBody TimeSpanEvent event
     ) {
-        var group = groupMapper.getGroupById(id);
-        if (group == null) {
-            return notFound("请求的用户不存在");
-        }
-
-        var oldEvent = eventMapper.getEventById(event.getId());
-
-        if (oldEvent == null) {
-            return notFound("要删除的事件不存在");
-        }
-
-        if (!event.equals(oldEvent)) {
-            return badRequest("要删除的事件信息同数据库不符");
+        if (id != event.getGroupId()) {
+            return badRequest("请求的组织ID和事件组织ID不一致");
         }
 
         try {
-            groupTimeSpanEventService.removeEvent(event);
+            timeSpanEventService.removeGroupEvent(event);
             return noContent();
         } catch (TimeSpanEventException e) {
             return badRequest(e.getMessage());
--- src/main/java/top/rrricardo/postcalendarbackend/services/TimeSpanEventService.java
+++ src/main/java/top/rrricardo/postcalendarbackend/services/TimeSpanEventService.java
@@ -1,35 +1,56 @@
 package top.rrricardo.postcalendarbackend.services;
 
-import top.rrricardo.postcalendarbackend.exceptions.AvlNodeRepeatException;
 import top.rrricardo.postcalendarbackend.exceptions.TimeSpanEventException;
 import top.rrricardo.postcalendarbackend.models.TimeSpanEvent;
-import top.rrricardo.postcalendarbackend.utils.generic.AvlTree;
-import top.rrricardo.postcalendarbackend.utils.generic.CustomHashTable;
 import top.rrricardo.postcalendarbackend.utils.generic.CustomList;
 
 import java.time.LocalDateTime;
 
-public abstract class TimeSpanEventService {
+public interface TimeSpanEventService {
     /**
-     * 添加一个时间段
+     * 添加一个用户的时间段事件
      *
      * @param event 需要添加的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
      */
-    public abstract void addEvent(TimeSpanEvent event) throws TimeSpanEventException;
+    void addUserEvent(TimeSpanEvent event) throws TimeSpanEventException;
 
     /**
-     * 移除一个事件
+     * 添加一个组织的时间段事件
+     * @param event 需要添加的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
+     */
+    void addGroupEvent(TimeSpanEvent event) throws TimeSpanEventException;
+
+    /**
+     * 移除一个用户的时间段事件
      *
      * @param event 需要移除的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
      */
-    public abstract void removeEvent(TimeSpanEvent event) throws TimeSpanEventException;
+    void removeUserEvent(TimeSpanEvent event) throws TimeSpanEventException;
 
     /**
-     * 更新事件
+     * 移除一个组织的时间段事件
+     * @param event 需要移除的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
+     */
+    void removeGroupEvent(TimeSpanEvent event) throws TimeSpanEventException;
+
+    /**
+     * 更新一个用户的时间段事件
      *
      * @param event 需要更新的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
      */
-    public abstract void updateEvent(TimeSpanEvent event) throws TimeSpanEventException;
+    void updateUserEvent(TimeSpanEvent event) throws TimeSpanEventException;
+
+    /**
+     * 更新一个组织的时间段时间
+     * @param event 需要更新的事件
+     * @throws TimeSpanEventException 事件冲突等情况引发的异常
+     */
+    void updateGroupEvent(TimeSpanEvent event) throws TimeSpanEventException;
 
     /**
      * 获得指定用户在指定时间范围的事件
@@ -39,176 +60,15 @@ public abstract class TimeSpanEventService {
      * @param end   结束时间
      * @return 事件列表
      */
-    public abstract CustomList<TimeSpanEvent> queryEvent(int id, LocalDateTime begin, LocalDateTime end) throws TimeSpanEventException;
-
-    public abstract boolean judgeConflict(int id, TimeSpanEvent event);
+    CustomList<TimeSpanEvent> queryUserEvent(int id, LocalDateTime begin, LocalDateTime end) throws TimeSpanEventException;
 
     /**
-     * 添加事件辅助函数
-     *
-     * @param event  需要添加的时间
-     * @param forest 需要添加的森林
-     * @param id     欲添加的属主ID
+     * 获得指定组织在指定时间范围内的时间
+     * @param id 组织ID
+     * @param begin 开始时间
+     * @param end 结束时间
+     * @return 事件列表
+     * @throws TimeSpanEventException 可能引发的各种错误
      */
-    protected void addEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest, int id)
-            throws TimeSpanEventException {
-        try {
-            var tree = forest.get(id);
-
-            if (tree == null) {
-                // 存在在应用启动之后
-                // 创建新用户的情况
-                tree = new AvlTree<>();
-                tree.insert(event);
-                forest.put(id, tree);
-            } else {
-                tree.insert(event);
-            }
-        } catch (AvlNodeRepeatException e) {
-            throw new TimeSpanEventException("发生冲突");
-        }
-    }
-
-    /**
-     * 删除事件辅助函数
-     *
-     * @param event  需要删除的事件
-     * @param forest 需要删除的森林
-     * @param id     属主ID
-     */
-    protected void removeEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
-                                     int id) throws TimeSpanEventException {
-        var tree = forest.get(id);
-
-        if (tree == null) {
-            throw new TimeSpanEventException("欲删除的事件非法：ID：" + id + "不存在");
-        }
-
-        tree.remove(event);
-    }
-
-    /**
-     * 修改事件辅助函数
-     *
-     * @param event  需要修改的事件
-     * @param forest 需要修改的森林
-     * @param id     属主ID
-     */
-    protected void updateEventHelper(TimeSpanEvent event, CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
-                                     int id) throws TimeSpanEventException {
-        var tree = forest.get(id);
-
-        if (tree == null) {
-            throw new TimeSpanEventException("欲删除的事件非法：ID：" + id + "不存在");
-        }
-
-        TimeSpanEvent oldEvent = null;
-
-        for (var item : tree) {
-            if (item.getId() == event.getId()) {
-                oldEvent = item;
-                break;
-            }
-        }
-
-        if (oldEvent == null) {
-            throw new TimeSpanEventException("欲修改的事件不存在");
-        }
-
-        if (oldEvent.getBeginDateTime().equals(event.getBeginDateTime())
-                && oldEvent.getEndDateTime().equals(event.getEndDateTime())) {
-            // 不修改事件的开始和结束时间
-            // 直接修改原来的二叉树
-            oldEvent.setName(event.getName());
-            oldEvent.setDetails(event.getDetails());
-            oldEvent.setPlaceId(event.getPlaceId());
-        } else {
-            // 修改事件的开始和结束时间
-            // 那就删除事件重新添加
-            tree.remove(oldEvent);
-            try {
-                tree.insert(event);
-            } catch (AvlNodeRepeatException exception) {
-                throw new TimeSpanEventException("发生冲突");
-            }
-        }
-    }
-
-    /**
-     * 查询事件辅助函数
-     *
-     * @param forest 查询的森林
-     * @param id     属主ID
-     * @param begin  开始时间
-     * @param end    结束时间
-     * @return 在时间段内的事件列表
-     * @throws TimeSpanEventException 查询不存在的用户/组织引发的错误
-     */
-    protected CustomList<TimeSpanEvent> queryEventHelper(CustomHashTable<Integer, AvlTree<TimeSpanEvent>> forest,
-                                                         int id, LocalDateTime begin, LocalDateTime end)
-            throws TimeSpanEventException {
-        var tree = forest.get(id);
-
-        if (tree == null) {
-            throw new TimeSpanEventException("属主：" + id + "不存在");
-        }
-
-        var beginTimeObj = new TimeSpanEvent();
-        beginTimeObj.setBeginDateTime(begin);
-        beginTimeObj.setEndDateTime(begin);
-
-        var endTimeObj = new TimeSpanEvent();
-        endTimeObj.setBeginDateTime(end);
-        endTimeObj.setEndDateTime(end);
-
-        return tree.selectRange(beginTimeObj, endTimeObj);
-    }
-
-    protected boolean judgeConflictHelper(TimeSpanEvent event, AvlTree<TimeSpanEvent> tree) {
-        var iterator = tree.iterator();
-
-        TimeSpanEvent left;
-        TimeSpanEvent right = null;
-
-        if (iterator.hasNext()) {
-            left = iterator.next();
-        } else {
-            // 在树中不存在元素
-            return true;
-        }
-
-        if (iterator.hasNext()) {
-            right = iterator.next();
-        }
-
-        if (event.compareTo(left) <= 0) {
-            // 事件在树中事件的前面
-            return event.getEndDateTime().isBefore(left.getBeginDateTime());
-        }
-
-        while (iterator.hasNext()) {
-            if (left.compareTo(event) <= 0 && event.compareTo(right) <= 0) {
-                // 找到位于中间的状态
-                return left.getEndDateTime().isBefore(event.getBeginDateTime())
-                        && event.getEndDateTime().isBefore(right.getBeginDateTime());
-            }
-
-            left = right;
-            right = iterator.next();
-        }
-
-        // 最后两个元素
-        // 当然还有还要考虑树中只有一个元素的情况
-        if (right != null) {
-            if (left.compareTo(event) <= 0 && event.compareTo(right) <= 0) {
-                // 找到位于中间的状态
-                return left.getEndDateTime().isBefore(event.getBeginDateTime())
-                        && event.getEndDateTime().isBefore(right.getBeginDateTime());
-            } else {
-                return right.getEndDateTime().isBefore(event.getBeginDateTime());
-            }
-        } else {
-            return left.getEndDateTime().isBefore(event.getBeginDateTime());
-        }
-    }
+    CustomList<TimeSpanEvent> queryGroupEvent(int id, LocalDateTime begin, LocalDateTime end) throws TimeSpanEventException;
 }
