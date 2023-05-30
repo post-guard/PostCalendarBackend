bebd19c7645872ef38138b205babdfb9c29cd134
jackfiled <xcrenchangjun@outlook.com>
2023-05-30 13:41:57 +0800

    add: 搜索前缀DTO

---
+++ src/main/java/top/rrricardo/postcalendarbackend/dtos/SearchPrefixDTO.java
@@ -0,0 +1,16 @@
+package top.rrricardo.postcalendarbackend.dtos;
+
+/**
+ * 搜索前缀DTO
+ */
+public class SearchPrefixDTO {
+    private String prefix;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+}
